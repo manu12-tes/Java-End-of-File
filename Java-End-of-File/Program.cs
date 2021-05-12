6158b760b2efa61831556cd76b97f4602dc3d66a
Manuel Garcia <kiramanuel123@gmail.com>
2021-05-12 01:27:26 -0500

    Agregar archivos de proyecto.

---
+++ Java-End-of-File/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Java_End_of_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            while (x < 3)
+            {
+                String st = Console.ReadLine();
+                Console.WriteLine(x + st);
+                x++;
+            }
+        }
+    }
+}
